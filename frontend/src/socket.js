99feabb1fa4c59f302450b59a99e8e0661d6cea5
Ashish Sahu <ashishsahu0052@gmail.com>
2026-05-29 01:38:30 +0530

    version 2.3.1

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Connect to the backend on port 3000
-const socket = io("http://localhost:3000", {
+const socket = io("https://picguesser.onrender.com", {
   autoConnect: false, // Connect manually when the user joins a room to prevent idle connections
 });
 
